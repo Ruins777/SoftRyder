de39ca5201c504e58cb9a897b2701b1bc0008442
rohit <rohit.wable9@gmail.com>
2022-11-11 12:53:07 +0530

    Employeedata changes updated

---
+++ JavaProgramsPractice/src/EmployeeData.java
@@ -0,0 +1,40 @@
+public class EmployeeData {
+
+    String name;
+    int emp_id;
+    int salary;
+    String position;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
