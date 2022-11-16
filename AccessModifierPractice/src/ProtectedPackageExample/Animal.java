28affbb25444d285942c14a31bf23fc3065854b4
rohit <rohit.wable9@gmail.com>
2022-11-16 21:11:32 +0530

    Changes: This is Second day class-Created Access Modifier protected example created Changed By: Rohit Wable Changed Date: 16/11/2022

---
+++ AccessModifierPractice/src/ProtectedPackageExample/Animal.java
@@ -0,0 +1,9 @@
+package ProtectedPackageExample;
+
+public class Animal {
+
+    protected void display()
+    {
+        System.out.println("This is a protected keyword example: Animal");
+    }
+}
---
+++ AccessModifierPractice/src/ProtectedPackageExample/Dog.java
@@ -0,0 +1,12 @@
+package ProtectedPackageExample;
+
+public class Dog extends Animal {
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        d.display();
+    }
+
+
+}
